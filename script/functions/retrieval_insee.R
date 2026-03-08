65c70f826f730520c7cb678dbb80c6f9be4b3393
etiennekubiak <etienne.kubiak@ensae.fr>
2026-03-08 15:13:17 +0000

    evol

--- script/functions/retrieval_insee.R
+++ script/functions/retrieval_insee.R
@@ -22,3 +22,35 @@ get_insee <- function(ids) {
       tidyr::drop_na(time)
   })
 }
+
+get_insee_advanced <- function(ids){
+  
+ data <- get_insee(ids)
+ data$name <- NA
+ 
+ data$name <- ifelse(data$series_key == "011794755", "epargne2", data$name)
+ data$name <- ifelse(data$series_key == "011794746", "RDB", data$name)
+ data$name <- ifelse(data$series_key == "011794859", "PIB", data$name)
+ data$name <- ifelse(data$series_key == "011794805", "DP", data$name)
+ data$name <- ifelse(data$series_key == "011794792", "FBCF", data$name)
+ data$name <- ifelse(data$series_key == "001656164", "defaillances", data$name)
+ data$name <- ifelse(data$series_key == "011794733", "EBE", data$name)
+ data$name <- ifelse(data$series_key == "001565530", "climat_affaires", data$name)
+
+ 
+ data <- data %>%
+     tidyr::pivot_wider(
+       id_cols = time,
+     names_from = name,
+     values_from = values
+   )
+
+ data <- data %>%
+   filter(time >= as.Date("1999-03-01") & time <= as.Date("2025-06-01"))
+ 
+ return(data)
+}
+
+a <- get_insee_advanced(c("001656164","011794733"))
+
+View(a)
